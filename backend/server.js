b890dd085422a6dbd968c5b09c8171e9b9415334
Adi Mordo <adids1221@gmail.com>
2021-07-30 02:12:51 +0300

    Created mongo data models

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import products from './data/products.js'
+import colors from 'colors'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
 
-const app = express()
 dotenv.config()
+connectDB()
+const app = express()
 
 app.get('/', (req, res) => {
     res.send('Api is runing')
