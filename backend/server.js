301a7a4254e5ea294c1ed653521614a8676f10e6
Adi Mordo <adids1221@gmail.com>
2021-07-30 16:38:21 +0300

    Initialize Redux, products reduces and actions

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import products from './data/products.js'
 import colors from 'colors'
 import dotenv from 'dotenv'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
+
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 connectDB()
@@ -12,14 +14,10 @@ app.get('/', (req, res) => {
     res.send('Api is runing')
 })
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-})
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
--- frontend/src/components/Rating.jsx
+++ frontend/src/components/Rating.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Rating = ({ value, text, color }) => {
-  console.log(value);
+  //console.log(value);
   return (
     <div className='rating'>
       <span>
